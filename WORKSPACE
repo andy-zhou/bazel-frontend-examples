ad34bab03eb89aa80c4ca2d416ef9cc40c2e7a8b
Andrew Zhou <andy@arithos.com>
2020-08-02 12:15:30 -0700

    played around with Bazel building nestjs

--- WORKSPACE
+++ WORKSPACE
@@ -7,8 +7,8 @@ workspace(
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 http_archive(
     name = "build_bazel_rules_nodejs",
-    sha256 = "c612d6b76eaa17540e8b8c806e02701ed38891460f9ba3303f4424615437887a",
-    urls = ["https://github.com/bazelbuild/rules_nodejs/releases/download/0.42.1/rules_nodejs-0.42.1.tar.gz"],
+    sha256 = "0f2de53628e848c1691e5729b515022f5a77369c76a09fbe55611e12731c90e3",
+    urls = ["https://github.com/bazelbuild/rules_nodejs/releases/download/2.0.1/rules_nodejs-2.0.1.tar.gz"],
 )
 load("@build_bazel_rules_nodejs//:index.bzl", "node_repositories", "yarn_install")
 node_repositories(
--- nextjs-app/next-build.js
+++ nextjs-app/next-build.js
@@ -7,15 +7,35 @@
 const path = require("path");
 const minimist = require("minimist");
 const nextBuild = require("next/dist/build").default;
+const fs = require("fs");
+const promisify = require("util").promisify;
+const ncp = promisify(require("ncp").ncp);
+const rimraf = promisify(require("rimraf"));
 
-main(process.argv.slice(2)).catch(err => {
+// Set copy concurrency limit
+ncp.limit = 16;
+
+main(process.argv.slice(2)).catch((err) => {
   console.error(err);
   process.exitCode = 1;
 });
 
 async function main(argv) {
   const args = minimist(argv);
-  await nextBuild(path.resolve(args.src_dir), {
-    distDir: path.resolve(args.out_dir)
-  });
+
+  const srcPath = path.resolve(args.src_dir);
+  const outPath = path.resolve(args.out_dir);
+  const copiedSrcPath = path.join(outPath, "src");
+  if (!fs.existsSync(copiedSrcPath)) {
+    fs.mkdirSync(copiedSrcPath);
+  }
+
+  console.log('Preparing next build...')
+  await ncp(srcPath, copiedSrcPath);
+
+  console.log("Starting next build...")
+  await nextBuild(outPath);
+
+  console.log('Cleaning up next build...')
+  await rimraf(copiedSrcPath)
 }
